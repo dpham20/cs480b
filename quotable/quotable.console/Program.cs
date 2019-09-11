b228003f66c4242c54dd40ed6159731d87d25e02
Pham <dpham20@jcu.edu>
2019-09-11 15:09:26 -0400

    update #2 HW0

--- quotable/quotable.console/Program.cs
+++ quotable/quotable.console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using quotable.core;
 
 namespace quotable.console
 {
@@ -7,6 +8,8 @@ namespace quotable.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            SimpleRandomQuoteProvider test = new SimpleRandomQuoteProvider();
+            test.returnQ(10);
             Console.ReadKey();
         }
     }
--- quotable/quotable.core/SimpleRandomQuoteProvider.cs
+++ quotable/quotable.core/SimpleRandomQuoteProvider.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 namespace quotable.core{
     public class SimpleRandomQuoteProvider : RandomQuoteProvider{
         public IEnumerable<string> returnQ (long numOfQuotes){
-            return null;
+            IEnumerable<string> test;
+            var testArr = new string[]{"Quote1", "Quote2", "Quote3", "TwoDollah"};
+            return testArr;
         }
     }
 }
